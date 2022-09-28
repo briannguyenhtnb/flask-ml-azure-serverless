d7347e939aced91bb3301ca142924de9def23dad
brian <nghiahtnb@gmail.com>
2022-09-28 00:25:32 +0000

    update the text in the home page to see the updte will be deployed by Azure pipelines

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home</h3>"
+    html = "<h3>Sklearn Prediction Home update (Will be deployed from Azure piplines)</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
